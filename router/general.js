279f80d25ae15b0629e2c4b8f1fce62ddbbcaa96
Will L. <will@willlee.dev>
2024-05-07 17:49:51 -0600

    Implement endpoint for registering a new user

--- router/general.js
+++ router/general.js
@@ -5,8 +5,27 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req, res) => {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    if (!isValid(username)) {
+      users.push({
+        username: username,
+        password: password,
+      });
+      console.log(users);
+      return res.status(200).json({
+        message: "User has been succesfully registered.",
+      });
+    } else {
+      return res.status(400).json({
+        message: "User already exists.",
+      });
+    }
+  }
+
+  return res.status(404).json({ message: "Unable to register the user." });
 });
 
 // Get the book list available in the shop
