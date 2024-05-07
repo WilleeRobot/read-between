fa7145b721c8c472b5f14f53d3693f9b81385339
Will L. <will@willlee.dev>
2024-05-07 16:11:26 -0600

    Implement endpoint for retrieving reviews on a specific book ISBN

--- router/general.js
+++ router/general.js
@@ -87,8 +87,16 @@ public_users.get("/title/:title", function (req, res) {
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  if (books[isbn]) {
+    if (Object.keys(books[isbn].reviews).length === 0) {
+      return res.status(200).json({ message: "No reviews for this book yet." });
+    } else {
+      return res.status(200).json(books[isbn].reviews);
+    }
+  }
+
+  return res.status(404).json({ message: "Book not found" });
 });
 
 module.exports.general = public_users;
